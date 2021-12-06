0242387c2069987b2fc8c26bf8c359832e318bc6
debsndkh <93278466+debsndkh@users.noreply.github.com>
2021-12-07 00:17:02 +0800

    Delete speakHello.js

--- speakHello.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-	var speakWord = "Hello";
-	var HelloSpeaker = function (name) {
-		console.log(speakWord + " " + name);
-	}
-
-	window.HelloSpeaker = HelloSpeaker;
-
-})(window);
