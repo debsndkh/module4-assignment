cbc0a472621b8b667df821790379fdbe819acd42
debsndkh <93278466+debsndkh@users.noreply.github.com>
2021-12-07 00:16:34 +0800

    Delete speakGoodBye.js

--- speakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-	var speakWord = "Good Bye";
-	var ByeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.ByeSpeaker = ByeSpeaker;
-
-})(window);
